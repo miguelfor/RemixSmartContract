28e22f69e6b900402376e53214149e0c0c03c4b5
Miguel Forero <miguelfor@hotmail.com>
2018-10-02 14:35:44 -0500

    First SmartContract

---
+++ MessageStore.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.24;
+
+contract MessageStore {
+
+    address private owner;
+    string private message;
+
+    constructor() public {
+        owner = msg.sender;
+        
+    }
+    
+    
+    function setMessage(string newMessage) public {
+    message = newMessage;
+        
+    }
+    
+    function getMessage() public view returns (string){
+        return message;
+    }
+    
+}
